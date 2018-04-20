a3f7f1532948ae6c77bf9252016fbaf193ba9f9d
DESKTOP-A6GUDI8\aarel <aarellano@techtonicgroup.com>
2018-04-20 11:49:18 -0600

    added program.cs change

--- ExcelConversion/Program.cs
+++ ExcelConversion/Program.cs
@@ -1,26 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ExcelConversion
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string mapInPath = @"D:\ExcelConversion\ExcelConversion\InputMapForXlsm.xlsx";
-            string fileInPath = @"D:\ExcelConversion\ExcelConversion\ExcelConversion\SEPs2.0\All-SEPs";
-            string fileOutPath = @"D:\ExcelConversion\ExcelConversion\ExcelConversion\SEPs2.0\SEPs";
+            string fileOutPath = "";
+            string fileRootPath = Directory.GetParent(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).ToString()).ToString();
+            List<string> files = System.IO.Directory.GetFiles(fileRootPath, "*.xlsm").ToList();
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                if (File.Exists(file))
+                {
+                    // This path is a file
+                    ProcessFile(file);
+                }
+                else if (Directory.Exists(file))
+                {
+                    // This path is a directory
+                    ProcessDirectory(file);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not a valid file or directory.", file);
+                }
+            }
             //instaniate class
             ConvertExcel convert = new ConvertExcel();
             //read map
             List<MapVal> map = convert.ReadMapFile(mapInPath);
             //read from excel
-            StringBuilder output = convert.ReadInfoFromExcel(fileInPath, map);
+            StringBuilder output = convert.ReadInfoFromExcel(fileRootPath, map);
             //write out to CSV
             convert.WriteToCSV(output, fileOutPath);
+        }
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public static void ProcessFile(string file)
+        {
+            Console.WriteLine("Processed file '{0}'.", file);
         }
     }
 }
